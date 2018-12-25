f3c347eed0d4ec3b578353e73828c79eb153fffe
s7ev3n <s.eleven.73@gmail.com>
2018-12-25 10:15:03 +0800

    fixed untracked files

--- ros/build/localization/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/smartcar/Harddisk/ubundisk/autonomous_carla/ros/src/localization
-# Build directory: /home/smartcar/Harddisk/ubundisk/autonomous_carla/ros/build/localization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros/build/perception/segmentation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/smartcar/Harddisk/ubundisk/autonomous_carla/ros/src/perception/segmentation
-# Build directory: /home/smartcar/Harddisk/ubundisk/autonomous_carla/ros/build/perception/segmentation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
